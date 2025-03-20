8bb4666cfcb0f1910572165656617b7409c98156
Henintsoa Paul <henintsoapaul@gmail.com>
2025-03-21 00:28:31 +0300

    [fix][calendar]-Fix google calendar api exception on events without begin and end times

--- src/main/java/site/easy/to/build/crm/google/model/calendar/EventDateTime.java
+++ src/main/java/site/easy/to/build/crm/google/model/calendar/EventDateTime.java
@@ -1,25 +1,10 @@
 package site.easy.to.build.crm.google.model.calendar;
 
+import lombok.Data;
+
+@Data
 public class EventDateTime {
-//    private String date;
+    private String date;
     private String dateTime;
-
     private String timeZone;
-    // Getters and setters
-
-    public String getDateTime() {
-        return dateTime;
-    }
-
-    public void setDateTime(String dateTime) {
-        this.dateTime = dateTime;
-    }
-
-    public String getTimeZone() {
-        return timeZone;
-    }
-
-    public void setTimeZone(String timeZone) {
-        this.timeZone = timeZone;
-    }
 }
