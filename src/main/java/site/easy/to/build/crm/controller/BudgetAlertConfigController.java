6ab52e685e7af57fc6e6567f4418c42d1cc33473
Henintsoa Paul <henintsoapaul@gmail.com>
2025-03-22 20:42:36 +0300

    [api][budget_alert]-Respond with correct response API

--- src/main/java/site/easy/to/build/crm/controller/BudgetAlertConfigController.java
+++ src/main/java/site/easy/to/build/crm/controller/BudgetAlertConfigController.java
@@ -1,9 +1,13 @@
 package site.easy.to.build.crm.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import site.easy.to.build.crm.api.ApiBadResponse;
+import site.easy.to.build.crm.api.ApiOkResponse;
+import site.easy.to.build.crm.api.ApiResponse;
 import site.easy.to.build.crm.entity.BudgetAlertConfig;
 import site.easy.to.build.crm.service.BudgetAlertConfigService;
 
@@ -24,7 +28,18 @@ public class BudgetAlertConfigController {
     }
 
     @PostMapping
-    public ResponseEntity<BudgetAlertConfig> create(@RequestParam double rate) {
-        return ResponseEntity.ok(budgetAlertConfigService.save(rate));
+    public ResponseEntity<ApiResponse<?>> create(@RequestParam double rate) {
+        ApiResponse<?> response;
+
+        boolean isValid = rate >= 0 && rate <= 100;
+        if (!isValid) {
+            response = new ApiBadResponse<>("Valeur hors borne! Rate must be between 0 and 100");
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        }
+
+        BudgetAlertConfig bac = budgetAlertConfigService.save(rate);
+
+        response = new ApiOkResponse<>("Insertion ok", bac);
+        return ResponseEntity.ok(response);
     }
 }
