6c75dc9517ae08b7a366c6f93073f968b34b5e4e
Henintsoa Paul <henintsoapaul@gmail.com>
2025-03-22 20:33:38 +0300

    [api][dto]-Add generic api response templates

---
+++ src/main/java/site/easy/to/build/crm/api/ApiBadResponse.java
@@ -0,0 +1,26 @@
+package site.easy.to.build.crm.api;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.http.HttpStatus;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class ApiBadResponse<T> extends ApiResponse<T> {
+
+    public ApiBadResponse(String message, T data) {
+        super(
+                HttpStatus.BAD_REQUEST.value(),
+                message,
+                data
+        );
+    }
+
+    public ApiBadResponse(int status, String message, T data) {
+        super(
+                status,
+                message,
+                data
+        );
+    }
+}
