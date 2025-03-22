16cd0425a2d386af007312137a77832ffb6d6bc0
Henintsoa Paul <henintsoapaul@gmail.com>
2025-03-22 19:05:40 +0300

    [budget_alert][fix]-Fix query for getting the current config

--- src/main/java/site/easy/to/build/crm/repository/BudgetAlertConfigRepository.java
+++ src/main/java/site/easy/to/build/crm/repository/BudgetAlertConfigRepository.java
@@ -7,6 +7,6 @@ import site.easy.to.build.crm.entity.BudgetAlertConfig;
 
 @Repository
 public interface BudgetAlertConfigRepository extends JpaRepository<BudgetAlertConfig, Integer> {
-    @Query("select b from BudgetAlertConfig b order by b.id limit 1")
+    @Query("select b from BudgetAlertConfig b order by b.id desc limit 1")
     BudgetAlertConfig findCurrent();
 }
